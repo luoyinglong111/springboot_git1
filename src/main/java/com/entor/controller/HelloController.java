549fd6da3dd5f0d7e14b700dd3f7518ab1821c03
LYL <1243077293@qq.com>
2020-11-30 16:30:42 +0800

    获取服务器端口

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+    @GetMapping("/get")
+    public String get(){
+        return "hello world"+port;
     }
 }
